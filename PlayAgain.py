de289a6c3b42ac7b5a8572d84ae42c422ad64e71
priyamorf <62360923+priyamorf@users.noreply.github.com>
2020-04-23 01:15:45 -0400

    Add files via upload

---
+++ PlayAgain.py
@@ -0,0 +1,8 @@
+def playagain():
+	answer == input("Do you want to play again? Y or N? ")
+	if answer == "Y" or answer == "y":
+		print("Let's start over!")
+		scene1()
+
+	elif answer == "N" or answer == "n":
+		print("See you!")
