cabb1e45fa5a22e0bbc1c825456103550b770d94
priyamorf <62360923+priyamorf@users.noreply.github.com>
2020-04-24 15:15:15 -0400

    Create FullCode.py

---
+++ FullCode.py
@@ -0,0 +1,52 @@
+def playagain():
+	answer == input("Do you want to play again? Y or N? ")
+	if answer == "Y" or answer == "y":
+		print("Let's start over!")
+		scene1()
+
+	elif answer == "N" or answer == "n":
+		print("See you!")
+    
+
+def scene1():
+
+
+def scene2():
+
+
+def scene3():
+
+def scene4():
+	print("After hearing about the ball from your sisters, you decide to ask your stepmother to attend as well.")
+	print("... story continues ...")
+
+	decision = input("Do you... \n A. Decide against going, as your stepmother wishes. \n B. Decide to go secretly anyways. \n C. Confront your mother and tell her that you're going regardless of her opinion.\n")
+
+	if decision == "A":
+		print("... story continues ...")
+		points += 0
+		scene5()
+
+	elif decision == "B":
+		print("... story continues ...")
+		points += 1
+		scene6()
+
+	elif decision == "C":
+		print("... story continues ...")
+		points += 2
+		scene6()
+
+def scene5():
+
+
+def scene6():
+
+
+def scene7():
+
+
+def scene8():
+
+
+def scene9():
